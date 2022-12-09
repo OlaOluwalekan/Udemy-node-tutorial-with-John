0bd7fd8d20bb24e1a0c989a5cc74eeef9276cd9a
OlaOluwalekan <olalekanbello534@gmail.com>
2022-12-09 22:22:17 +0100

    startde working with mongoose schema and models

---
+++ 03-task-manager/controllers/tasks.js
@@ -0,0 +1,29 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(200).json(task);
+};
+const getTask = (req, res) => {
+  const { taskId } = req.params;
+  console.log(req.params);
+  res.send("single task with id " + req.params.id);
+};
+const updateTask = (req, res) => {
+  res.send("edit task with id " + req.params.id);
+};
+const deleteTask = (req, res) => {
+  res.send("delete task with id " + req.params.id);
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
