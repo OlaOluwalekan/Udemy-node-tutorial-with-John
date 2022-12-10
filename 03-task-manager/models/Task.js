0cba024964ad1ea656b3b05c3437c179a9f98a82
OlaOluwalekan <olalekanbello534@gmail.com>
2022-12-10 08:03:49 +0100

    set up controllers for all task and single task

--- 03-task-manager/models/Task.js
+++ 03-task-manager/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "name is reqired"],
+    trim: true,
+    maxlength: [20, "maximum name length is 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
