26f191f39488c1920c0da1b607da88bdc0111b97
ImagineMe360 <imagine.media360@gmail.com>
2022-12-10 15:14:24 +0100

    complete all functionality for task manager app.
    Just left to refactor the codes

--- 03-task-manager/public/browser-app.js
+++ 03-task-manager/public/browser-app.js
@@ -51,9 +51,11 @@ showTasks();
 
 tasksDOM.addEventListener("click", async (e) => {
   const el = e.target;
+
   if (el.parentElement.classList.contains("delete-btn")) {
     loadingDOM.style.visibility = "visible";
     const id = el.parentElement.dataset.id;
+    console.log(id);
     try {
       await axios.delete(`/api/v1/tasks/${id}`);
       showTasks();
