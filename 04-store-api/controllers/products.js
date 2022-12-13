2d32a05bddf7c92a48a8244465b083fa388e746f
ImagineMe360 <imagine.media360@gmail.com>
2022-12-13 13:43:22 +0100

    continued with store api
    working on quering the database

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,10 +1,23 @@
+const Products = require("../models/Products");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async errors");
-  res.status(200).json({ msg: "testing product routes" });
+  // throw new Error("testing async errors");
+  const products = await Products.find({ name: "vase table" });
+  // const product = await Products.find((err,result)=>{
+
+  // });
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "product routes" });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === true ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Products.find(queryObject);
+  res.status(200).json({ products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
