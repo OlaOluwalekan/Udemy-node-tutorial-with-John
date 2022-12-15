6c02dcae49a66f75ddd0aee76b985b154fea776c
ImagineMe360 <imagine.media360@gmail.com>
2022-12-15 15:26:39 +0100

    completed store api project

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,23 +1,83 @@
 const Products = require("../models/Products");
 
 const getAllProductsStatic = async (req, res) => {
-  // throw new Error("testing async errors");
-  const products = await Products.find({ name: "vase table" });
-  // const product = await Products.find((err,result)=>{
+  const products = await Products.find({ price: { $gt: 30, $lt: 100 } })
+    .sort("price")
+    .select("name price");
 
-  // });
-  res.status(200).json({ products });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  console.log(req.query);
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
-    queryObject.featured = featured === true ? true : false;
+    queryObject.featured = featured === "true" ? true : false;
   }
+  if (company) {
+    // const companyList = ["ikea", "caressa", "marcos", "liddy"];
+    // queryObject.company = companyList.filter((co) => {
+    //   return co.includes(company);
+    // });
+
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  // NUMERIC FILTERS
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(numericFilters);
+    console.log(filters);
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   console.log(queryObject);
-  const products = await Products.find(queryObject);
-  res.status(200).json({ products });
+  let result = Products.find(queryObject);
+
+  // SORT
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  // SELECT
+  if (fields) {
+    const fielsList = fields.split(",").join(" ");
+    result = result.select(fielsList);
+  }
+
+  // PAGINATION
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ hitNo: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
